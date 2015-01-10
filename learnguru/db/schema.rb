90602e314f0c63de6e3844076a8062a12c9e5053
pdhancock <philipdhancock@gmail.com>
2015-01-10 14:25:03 +1100

    removed duplicate migration db:drop and migrate

--- learnguru/db/schema.rb
+++ learnguru/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110030922) do
+ActiveRecord::Schema.define(version: 20150110032149) do
 
   create_table "learnlists", force: :cascade do |t|
     t.string   "name"
