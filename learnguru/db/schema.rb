9a122f883f99a92c215097557ae99f8a53d517b2
Chris Ma <crispy-studios@hotmail.com>
2015-01-12 14:23:40 +1100

    active records for answers

--- learnguru/db/schema.rb
+++ learnguru/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112025135) do
+ActiveRecord::Schema.define(version: 20150112031620) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.string   "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "learnlists", force: :cascade do |t|
     t.string   "name"
