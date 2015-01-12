17968f9d6a25edb5f8463c02ce85565c0656d0d9
Chris Ma <crispy-studios@hotmail.com>
2015-01-12 14:12:38 +1100

    CRUD for quiz made

--- learnguru/app/controllers/quizzes_controller.rb
+++ learnguru/app/controllers/quizzes_controller.rb
@@ -8,4 +8,26 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.new
   end
 
+  def create
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
+      redirect_to quiz_path(@quiz)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @quiz = Quiz.find params[:id]
+    
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:name, :id)
+    
+  end
+
+
 end
--- learnguru/db/schema.rb
+++ learnguru/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112010635) do
+ActiveRecord::Schema.define(version: 20150112025135) do
 
   create_table "learnlists", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150112010635) do
   create_table "quizzes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
