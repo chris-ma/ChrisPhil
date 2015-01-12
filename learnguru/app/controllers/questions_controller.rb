9b03ed304a6a8747b6e5505739d99bf1baab055f
Chris Ma <crispy-studios@hotmail.com>
2015-01-12 13:57:17 +1100

    added name to quizzes

--- learnguru/app/controllers/questions_controller.rb
+++ learnguru/app/controllers/questions_controller.rb
@@ -4,8 +4,10 @@ class QuestionsController < ApplicationController
   def index
     @question = Question.all
   end
-  
+
   def new
     @question = Question.new
   end
+
+  
 end
--- learnguru/app/controllers/quizzes_controller.rb
+++ learnguru/app/controllers/quizzes_controller.rb
@@ -6,6 +6,6 @@ class QuizzesController < ApplicationController
 
   def new
     @quiz = Quiz.new
-    
   end
+
 end
