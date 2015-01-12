16d89c01d645aca8031b7df9ba9ca2a40296eb17
Chris Ma <crispy-studios@hotmail.com>
2015-01-12 12:07:23 +1100

    create the model for questions

--- learnguru/db/schema.rb
+++ learnguru/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111093718) do
+ActiveRecord::Schema.define(version: 20150112010635) do
 
   create_table "learnlists", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,20 @@ ActiveRecord::Schema.define(version: 20150111093718) do
     t.integer  "length"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "lessons_learnlists", force: :cascade do |t|
+    t.integer  "lesson_id"
+    t.integer  "learnlist_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "survey_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "quizzes", force: :cascade do |t|
