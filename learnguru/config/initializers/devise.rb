9b74622e792d1b341f6b2532aec744a0295d0fee
Chris Ma <crispy-studios@hotmail.com>
2015-01-13 15:37:43 +1100

    omniauth keys placed

--- learnguru/config/initializers/devise.rb
+++ learnguru/config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-
+  config.omniauth :linkedin, ENV["LINKEDIN_CONSUMER_KEY"], ENV["LINKEDIN_SECRET_KEY"]
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
