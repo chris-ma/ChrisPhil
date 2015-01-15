c7081aae75bc05542f9c8dc1bc1154145e85b42c
pdhancock <philipdhancock@gmail.com>
2015-01-16 10:07:33 +1100

    added zeroclipboard gem

--- learnguru/app/models/lesson.rb
+++ learnguru/app/models/lesson.rb
@@ -29,7 +29,8 @@ class Lesson < ActiveRecord::Base
   has_many :quizzes, dependent: :destroy
   accepts_nested_attributes_for :quizzes, allow_destroy: true
 
-  validates :provider_url, presence: true
+  # validates :provider_url, :html presence: true
+  validates_presence_of  :provider_url
 
   belongs_to :user
 
@@ -38,10 +39,10 @@ class Lesson < ActiveRecord::Base
     key = ENV['EMBEDLY_API_KEY']
     embeddetails = Tinyembedly::Oembed.new(key: key, url: url)
     embed = embeddetails.to_hash
-
     self.description = embed['description']
     self.title = embed['title']
     self.html = embed['html']
+    
     self.thumbnail_url = embed['thumbnail_url']
     self.author_name = embed['author_name']
     self.thumbnail_width = embed['embed_width']
@@ -50,10 +51,6 @@ class Lesson < ActiveRecord::Base
     self.media = embed['type']
     self.author_url = embed['author_url']
 
-    if self.media == 'video' || 'rich' then
-      response = HTTParty.get('http://api.embed.ly/1/extract?key='+key+'&url='+url)
-      self.duration = response['media']['duration']
-    end
   end
 
 
